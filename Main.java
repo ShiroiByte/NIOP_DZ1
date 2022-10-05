4ca9ea0335c6be38f13533bfe8d5fe47f56b0e76
Hypenss <79802072+ShiroiByte@users.noreply.github.com>
2022-10-05 12:58:21 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Phone phone1 = new Phone("Samsung S22",9000,true);
+
+        phone1.applyDiscount(50);
+    }
+}
---
+++ Phone.java
@@ -0,0 +1,51 @@
+public class Phone {
+    private String type;
+    private int price;
+    private boolean isAvailiable;
+
+    public Phone(String type, int price, boolean isAvailiable) {
+        this.type = type;
+        this.price = price;
+        this.isAvailiable = isAvailiable;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailiable() {
+        return isAvailiable;
+    }
+
+    public void setAvailiable(boolean availiable) {
+        isAvailiable = availiable;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "type='" + type + '\'' +
+                ", price=" + price +
+                ", isAvailiable=" + isAvailiable +
+                '}';
+    }
+
+    public void applyDiscount(int percantage){
+        System.out.println("With discount: " + (price)*percantage/100 + " HRK");
+
+    }
+
+
+}
